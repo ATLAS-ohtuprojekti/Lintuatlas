a1630ae717657831c6925802e1f4df47eba1e639
juliapalorinne <julia.palorinne.helsinki.fi>
2021-09-14 12:04:55 +0300

    React app created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Lintuatlas">
+        <h1>Lintuatlas</h1>
+        <p>Tähän tulee uusi lintuatlas.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
