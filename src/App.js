e51f6e4888542756743db93506d5bbb1970d8a60
juliapalorinne <julia.palorinne@helsinki.fi>
2021-10-12 11:08:39 +0300

    testing getting maps from Atlas API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+const App = (props) => {
+  const {species} = props
+  
   return (
     <div className="App">
       <header className="Lintuatlas">
         <h1>Lintuatlas</h1>
+
         <p>Tähän tulee uusi lintuatlas.</p>
+        <img 
+        src="https://atlas-staging.rahtiapp.fi/api/grid/map/data?id=25836"
+        alt="new"
+        />
       </header>
     </div>
   );
